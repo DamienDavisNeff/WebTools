5df8830141485094aaceaa18484241e42ae992e0
DamienDavisNeff <damiendavisneff@gmail.com>
2024-12-31 19:45:52 -0500

    youtube videos removal CLIENT

---
+++ remove_videos_by_keyword.js
@@ -0,0 +1,11 @@
+// This script removes videos from a youtube's channel page based on a keyword inserted by the user.
+// NOTE: The videos are removed on the CLIENT only
+// NOTE: The videos may return if the client refreshes in anyway, including resizing the page
+
+const keyword = prompt(); 
+document.querySelectorAll("ytd-rich-item-renderer").forEach(
+	item => { 
+		videoTitle = item.querySelector("#video-title-link").title;
+		if(videoTitle.includes(keyword)) item.remove();
+	}
+);
